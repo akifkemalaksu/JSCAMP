45bd95035926489fdd7d217e14650236bd711edf
akifkemalaksu <akifkemalaksu@gmail.com>
2021-12-11 17:03:29 +0300

    ders 3

--- src/components/userComponent.js
+++ src/components/userComponent.js
@@ -1,41 +1,18 @@
-import { BaseLogger, ElasticLogger, MongoLogger } from "../crossCuttingConcerns/logging/logger.js"
-import Customer from "../models/customer.js"
-import User from "../models/user.js"
-import UserService from "../services/userService.js"
+import { BaseLogger, MongoLogger } from "../crossCuttingConcerns/logging/logger.js";
+import User from "../models/user.js";
+import UserService from "../services/userService.js";
 
-console.log("User component yüklendi")
+console.log("User component loaded.");
 
-let logger1 = new MongoLogger()
-let userService = new UserService(logger1)
+let logger1 = new BaseLogger(); 
+let logger2 = new MongoLogger(); 
+let userSevice = new UserService(logger2);
 
-let user1 = new User(1,"Engin","Demiroğ","Ankara")
-let user2 = new User(2,"Baran","Gökçekli","Muğla")
-userService.add(user1)
-userService.add(user2)
-
-//console.log(userService.list())
-//console.log(userService.getById(2))
+let user1 = new User(1,"Akif Kemal","Aksu","İstanbul");
+let user2 = new User(2,"Emirhan","Aksu","İstanbul");
 
 
-
-
-let customer = {id:1, firstName:"Engin"}
-
-//prototyping
-customer.lastName = "Demiroğ"
-
-console.log(customer.lastName)
-
-console.log("--------------------------")
-userService.load()
-
-
-let customerToAdd = new Customer(1,"Seda","Yılmaz","Ankara","fdgdfg");
-customerToAdd.type = "customer"
-
-userService.add(customerToAdd)
-console.log(userService.customers)
-console.log(userService.employees)
-console.log(userService.errors)
-console.log(userService.getCustomersSorted())
-//22.00 Dersteyiz
+userSevice.add(user1);
+userSevice.add(user2);
+console.log(userSevice.getById(1));
+console.log(userSevice.list());
--- src/models/user.js
+++ src/models/user.js
@@ -1,9 +1,8 @@
-export default class User {
-    constructor(id, firstName, lastName, city,age) {
-        this.id = id
-        this.firstName = firstName
-        this.lastName = lastName
-        this.city = city
-        this.age = age
+export default class User{
+    constructor(id,firstName,lastName,city){
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.city = city;
     }
 }
